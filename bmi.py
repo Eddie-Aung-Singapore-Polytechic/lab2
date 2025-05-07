daa30bf06ae4dc3330e590e7d994685a299f1fd4
Eddie <AUNGKAUNG.24@ichat.sp.edu.sg>
2025-05-08 04:27:03 +0800

    Updated bmi.py

---
+++ bmi.py
@@ -0,0 +1,24 @@
+def calculate_bmi(height,weight):
+    
+    #Prints out the height and weight beforehand
+    print("Height = " + str(weight))
+    print("Weight = " +  str(height))
+    # Calculates BMI and displays it
+    bmi = weight / (height * height)
+    print("Body Mass Index (BMI): ", round(bmi,2))
+
+    # Determines the user's weight classification 
+    weight_class = "..."
+    if bmi < 18.5:
+        weight_class = "Under Weight"
+    elif bmi <= 25.0:
+        weight_class = "Normal Weight"
+    else:
+        weight_class = "Over weight"
+    print("The user is", weight_class)
+
+    return 0
+
+
+
+calculate_bmi(height=1.73,weight=57)
