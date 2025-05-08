3319aa7f17d586cb690501631935d29722f90154
Eddie <AUNGKAUNG.24@ichat.sp.edu.sg>
2025-05-08 16:04:46 +0800

    Lab3 exercise changes

--- bmi.py
+++ bmi.py
@@ -10,13 +10,16 @@ def calculate_bmi(height,weight):
     weight_class = "..."
     if bmi < 18.5:
         weight_class = "Under Weight"
+        return round(bmi,2), '-1'
     elif bmi <= 25.0:
         weight_class = "Normal Weight"
+        return round(bmi,2), '0'
     else:
         weight_class = "Over weight"
-    print("The user is", weight_class)
+        return round(bmi,2), '1'
 
-    return 0
+
+
 
 
 calculate_bmi(height=1.73,weight=57)
